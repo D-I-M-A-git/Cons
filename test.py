14659768a160245799f293932e5065ea2e6f17b5
D-I-M-A-git <102852972+D-I-M-A-git@users.noreply.github.com>
2022-07-26 20:55:59 +0300

    UPDATE

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from aiogram.utils import executor
 from time import sleep
 
 # Створення бота і глобального списку
-bot = Bot(token="Bot token")
+bot = Bot(token="BOT TOKEN")
 gp = Dispatcher(bot)
 global users_way
 users_way = {}
@@ -17,12 +17,12 @@ print('START BOT')
 @gp.message_handler()
 async def get_message(message: types.Message):
     chat_id = message.chat.id
-    if chat_id == 1909237932:  # Перевірка що юзер той
+    if chat_id == 1909237932 or chat_id == 1354921548:  # Перевірка що юзер той
         message_text = message.text.split()  # Розділення повідомлення в список
         match message_text:
             # Якщо повідомлення дорівнює gg або GG, то бот надсилає мемас
             case ["gg"] | ["GG"]:
-                sent_message = await bot.send_message(chat_id=chat_id, text="NO GG")
+                await bot.send_message(chat_id=chat_id, text="NO GG")
                 await bot.send_video(chat_id=chat_id, video="https://c.tenor.com/2jZjeGLb0McAAAAM/gg-no-re-rematch.gif")
             # Якщо повідомлення дорівнює словам Хто я?, то бот надсилає інформацію про юзера
             case ["Хто", "я?"]:
@@ -40,6 +40,7 @@ async def get_message(message: types.Message):
                     except:
                         print("ERROR")
                         sleep(11)
+            # Якщо повідомлення дорівнює get_photo, то бот надсилає фото на аватарці
             case ["get_photo"]:
                 user_photo = await bot.get_user_profile_photos(user_id=chat_id, offset=5)
                 for i in user_photo.iter_values():
@@ -64,12 +65,17 @@ async def get_message(message: types.Message):
                     print(i, "num 3")
                 user_photo = user_photo['file_id']
                 await bot.send_photo(chat_id=chat_id, photo=user_photo)
-            case ['photo', photo_name]:
-                try:
-                    with open(f"media/images/{photo_name}", 'rb') as photo:
-                        await bot.send_photo(chat_id=chat_id, photo=photo)
-                except:
-                    await bot.send_message(chat_id=chat_id, text="Файла нема!")
+
+            case ['photos', photo_name]:
+                if photo_name == "all":
+                    # try:
+                    photos = os.listdir(r"media\\images")
+                    print(photos, type(photos))
+                    for photo in photos:
+                        with open(f"media/images/{photo}", 'rb') as photo_file:
+                            await bot.send_photo(chat_id=chat_id, photo=photo_file, caption=photo)
+                    # except:
+                    #     await bot.send_message(chat_id=chat_id, text="Файлів нема!")
             case ['dir', 'photos']:
                 photos = '\n'.join(os.listdir(r"media\\images"))
                 await bot.send_message(chat_id=chat_id, text=photos)
@@ -80,31 +86,42 @@ async def get_message(message: types.Message):
                 except:
                     await bot.send_message(chat_id=chat_id, text="Файлів нема!")
             case ['@dir', '@back']:
-                del users_way[chat_id]
+                users_way[chat_id] = None
                 await bot.send_message(chat_id=chat_id, text="Бекап зроблений")
             case ['@dir', way]:
-                try:
-                    print("TRY:")
-                    file = users_way[chat_id]
-                    print(file, "numer 0")
-                    file += way
-                    file += r"\\"
-                    print(file, "numer 1")
-                    users_way[chat_id] = file
-                    way = file
-                    file = '\n'.join(os.listdir(file))
-                    await bot.send_message(chat_id=chat_id, text=f"<{way}>\n{file}")
-                except:
-                    print("EXCEPT:")
-                    users_way[chat_id] = way
-                    file = users_way[chat_id] = way
-                    print(file, "numer 2")
-                    file += r"\\"
-                    print(file, "numer 3")
-                    users_way[chat_id] = file
-                    way = file
-                    file = '\n'.join(os.listdir(file))
-                    await bot.send_message(chat_id=chat_id, text=f"<{way}>\n{file}")
+                if way == "my":
+                    await bot.send_message(chat_id=chat_id, text=f"Моє розположення\n{os.getcwd()}")
+                else:
+                    try:
+                        print("TRY:")
+                        file = users_way[chat_id]
+                        print(file, "numer 0")
+                        file += way
+                        file += r"\\"
+                        print(file, "numer 1")
+                        users_way[chat_id] = file
+                        way = file
+                        try:
+                            file = '\n'.join(os.listdir(file))
+                            await bot.send_message(chat_id=chat_id, text=f"<{way}>\n{file}")
+                        except:
+                            users_way[chat_id] = None
+                            await bot.send_message(chat_id=chat_id, text="Бекап зроблений")
+                    except:
+                        print("EXCEPT:")
+                        users_way[chat_id] = way
+                        file = users_way[chat_id] = way
+                        print(file, "numer 2")
+                        file += r"\\"
+                        print(file, "numer 3")
+                        users_way[chat_id] = file
+                        way = file
+                        try:
+                            file = '\n'.join(os.listdir(file))
+                            await bot.send_message(chat_id=chat_id, text=f"<{way}>\n{file}")
+                        except:
+                            users_way[chat_id] = None
+                            await bot.send_message(chat_id=chat_id, text="Бекап зроблений")
 
             case _:
                 sent_message = await bot.send_message(chat_id=chat_id, text=f'Я не розумію команду \"{message.text}\"')

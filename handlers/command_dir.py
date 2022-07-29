8a46ba7f46e6b907b8b0ee11ded9542717e80958
D-I-M-A-git <102852972+D-I-M-A-git@users.noreply.github.com>
2022-07-29 19:36:37 +0300

    Separating the function from the command_dir.py file

--- handlers/command_dir.py
+++ handlers/command_dir.py
@@ -2,6 +2,7 @@ import os
 
 from aiogram import types, Dispatcher
 
+from handlers import file_explorer_function
 from create_bot import bot
 import config as cnf
 
@@ -17,14 +18,9 @@ async def file_explorer(message: types.Message):
         message_text = message.text.split()  # Розділення повідомлення в список
         match message_text:
             # Виконує команди після @dir
-            case['@dir', *way]:
-                match way:
-                    # Коли юзер пише @back то бот видаляє інформацію про юзера
-                    case ["@back"]:
-                        del users_way[chat_id]
-                        await bot.send_message(chat_id=chat_id, text="Бекап зроблений")
+            case['@dir', *new_way]:
+                match new_way:
                     # Коли юзер пише @image txt то бот надсилає повідомлення файлів які знаходяться в media\image
-
                     case ["@image", "txt"]:
                         images = '\n'.join(os.listdir(r"media\\images"))
                         await bot.send_message(chat_id=chat_id, text=images)
@@ -48,30 +44,11 @@ async def file_explorer(message: types.Message):
                         await bot.send_message(chat_id=chat_id, text=f"Моє розположення\n{os.getcwd()}")
                     # Показує файли, папки через відповідний шлях
                     case _:
-                        try:
-                            file = users_way[user_id]
-                            file += way
-                            file += r"/"
-                            users_way[user_id] = ''.join(file)
-                            way = ''.join(file)
-                            try:
-                                files = '\n'.join(os.listdir(''.join(file)))
-                                await bot.send_message(chat_id=chat_id, text=f"<{way}>\n{files}")
-                            except:
-                                del users_way[user_id]
-                                await bot.send_message(chat_id=chat_id, text="Етап(1) Бекап зроблений через можливу помилку!")
-                        except:
-                            users_way[user_id] = way
-                            file = users_way[user_id]
-                            file += r"/"
-                            users_way[user_id] = file
-                            way = file
-                            try:
-                                files = '\n'.join(os.listdir(''.join(file)))
-                                await bot.send_message(chat_id=chat_id, text=f"<{way[0]}>\n{files}")
-                            except:
-                                del users_way[user_id]
-                                await bot.send_message(chat_id=chat_id, text='Етап(2) Бекап зроблений через можливу помилку!')
+                        way = " ".join(new_way)
+                        user_id_str = str(user_id)
+                        files = file_explorer_function.file_explorer_function(way, user_id_str)
+                        await bot.send_message(chat_id=chat_id, text=files)
+
             case _:
                 message_text = message.text.lower()
                 for word in cnf.WORDS:
---
+++ handlers/file_explorer_function.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+
+def file_explorer_function(way, user_id):
+    if way == "@back":
+        with open("data.json", "w") as new_data:
+            json.dump({user_id: "C:/"}, new_data)
+        return "Бекап зроблений!"
+    elif way == "@view":
+        with open("data.json", "r") as data:
+            data = json.load(data)
+        files = '\n'.join(os.listdir(data[user_id]))
+        message = f"<{data[user_id]}>\n" \
+                  f"{files}"
+        return message
+    else:
+        with open("data.json", "r") as data:
+            data = json.load(data)
+        way += "/"
+        data[user_id] += way
+        try:
+            files = os.listdir(data[user_id])
+            files = "\n".join(files)
+            with open("data.json", "w") as new_data:
+                json.dump(data, new_data)
+            message = f"<{data[user_id]}>\n" \
+                      f"{files}"
+            return message
+        except:
+            return f'Сталась помилка!\n<{data[user_id]}>'

6e7b0e43d28bff02c6864712107af9bbee918f41
D-I-M-A-git <102852972+D-I-M-A-git@users.noreply.github.com>
2022-09-21 20:50:51 +0300

    Update command_search.py

--- handlers/command_search.py
+++ handlers/command_search.py
@@ -1,20 +1,13 @@
-import requests
-from bs4 import BeautifulSoup as BS
+import wikipedia
+import translators as ts
 from aiogram import types, Dispatcher
 
 
 async def search(message: types.Message):
-    try:
-        url = message.text.replace("/search ", "")
-        r = requests.get("https://uk.wikipedia.org/wiki/" + url)
-        html = BS(r.content, 'lxml')
-        text = html.find("p").text.strip()
-        if text == "Інші причини, чому Ви можете бачити це повідомлення:":
-            await message.reply(text="Сталась помилка")
-        else:
-            await message.reply(text=text)
-    except:
-        await message.reply(text="Сталась помилка")
+    await message.reply(text="Секунду...")
+    text = message.text.replace("/search ", "").lower()
+    await message.reply(text=ts.google(wikipedia.summary(text), to_language='uk'))
+
 
 def register_handler_help(dp: Dispatcher):
     dp.register_message_handler(search, commands="search")
